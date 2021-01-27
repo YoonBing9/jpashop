05ed348b8c6d6a63f69271804337615993169547
bing9 <akass191@gmail.com>
2021-01-27 15:42:48 +0900

    Item 재고수량 예외 테스트 코드 추가

--- src/test/java/jpabook/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -2,11 +2,14 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
@@ -27,5 +30,22 @@ class ItemServiceTest {
 
         //then
         Assertions.assertThat(book).isEqualTo(itemService.findItemById(book.getId()));
+    }
+
+    @Test
+    public void 상품_재고수량_예외() throws Exception {
+        //given
+        Item book = new Book();
+        book.setName("JPA의 정석");
+        book.setPrice(30000);
+        book.setStockQuantity(10000);
+
+        //when
+
+
+        //then
+        assertThrows(NotEnoughStockException.class, () -> {
+            book.removeStock(10001);
+        });
     }
 }
