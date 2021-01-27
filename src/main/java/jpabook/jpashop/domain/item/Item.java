548e6c56e14d3828b11c8e88aed76714ac7f5352
bing9 <akass191@gmail.com>
2021-01-27 15:19:25 +0900

    Item Entity 재고수량 늘리기, 줄이기 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -23,4 +24,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories;
+
+    /**
+     * 재고수량 늘리기
+     */
+    public void addStock(int quantity) {
+        stockQuantity += quantity;
+    }
+
+    /**
+     * 재고수량 줄이기
+     */
+    public void removeStock(int quantity) {
+        int resultStock = stockQuantity - quantity;
+        if(resultStock < 0) {
+            throw new NotEnoughStockException("남은 재고가 충분하지 않습니다.");
+        }
+
+        stockQuantity = resultStock;
+    }
 }
